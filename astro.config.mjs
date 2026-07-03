a1350cfc545678dde4c85d6e18ac38f6464e8f53
lfm <lfm630>
2026-07-03 12:29:07 +0800

    add trailingSlash and output config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+const REPO_BASE = "/my-blog";
+
 export default defineConfig({
   site: "https://lfm630.github.io",
-  base: "/my-blog",
+  base: REPO_BASE,
+  trailingSlash: "always",
+  output: "static",
   integrations: [tailwind()],
 });
